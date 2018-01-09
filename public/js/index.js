8666b48b477e374b2651da38b1b9bc416f4529e5
Artem Goryachev <shrewmus@yandex.ru>
2018-01-09 05:16:06 +0300

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,26 @@
+/**
+ * @file index.js
+ * @author shrewmus (shrewmus@yandex.ru contact@shrewmus.name)
+ * Date: 1/9/18
+ * (c) 2018
+ */
+
+var socket = io();
+
+socket.on('connect', function() {
+    console.log('Connected to server');
+
+    socket.emit('createMessage', {
+        from: 'jen',
+        text: 'See you then'
+    });
+
+});
+
+socket.on('disconnect', function() {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+    console.log('newMessage', message);
+});
--- server/server.js
+++ server/server.js
@@ -21,9 +21,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'shrewmus',
+        text: 'All seems ok',
+        createdAt: 1231321
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+
     socket.on('disconnect', () => {
         console.log('User was disconnected');
-    })
+    });
+
 });
 
 
